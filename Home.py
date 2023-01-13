b53d66afad5324c8eb056df089ec5fdc14bcb0d5
Limited icu <icunethk@gmail.com>
2023-01-14 03:01:00 +0800

    fix

--- Home.py
+++ Home.py
@@ -1,4 +1,15 @@
 import streamlit as st
+from st_pages import Page, show_pages, add_page_title
+
+add_page_title()
+
+show_pages(
+    [
+        Page("streamlit_app.py", "Home", "🏠"),
+        Page("other_pages/page2.py", "Page 2", ":books:"),
+    ]
+)
+
 st.set_page_config(
     page_title="Home",
     page_icon="👋",
